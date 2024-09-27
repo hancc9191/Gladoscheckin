cc8c015fdc5b5e35599d260bc66d04a687487222
hancc9191 <105869123+hancc9191@users.noreply.github.com>
2024-09-27 11:22:24 +0800

    Update checkin.py

--- checkin.py
+++ checkin.py
@@ -73,8 +73,8 @@ if __name__ == '__main__':
     print("sendContent:" + "\n", sendContent)
     
     # Server酱推送
-    if sckey != "":
+    if SERVER_CHAN_SCKEY != "":
         title = f'成功{success},失败{fail},重复{repeats}'
-        server_chan_url = f"https://sctapi.ftqq.com/{sckey}.send?text={title}&desp={sendContent}"
+        server_chan_url = "https://sctapi.ftqq.com/{SERVER_CHAN_SCKEY}.send?text={title}&desp={sendContent}"
         r = requests.get(server_chan_url)
         print(r.status_code)
